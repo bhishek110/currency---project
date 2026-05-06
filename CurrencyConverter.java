7ab6549fe20a25c806142d0fdb29badef305649a
Abishek <bhishek110@gmail.com>
2026-05-06 22:21:14 +0530

    Initial commit

---
+++ CurrencyConverter.java
@@ -0,0 +1,14 @@
+public class CurrencyConverter {
+
+    public double rupeeToDollar(double rupee) {
+        return rupee / 83;
+    }
+
+    public double dollarToRupee(double dollar) {
+        return dollar * 83;
+    }
+
+    public double rupeeToEuro(double rupee) {
+        return rupee / 90;
+    }
+}
---
+++ CurrencyConverterTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CurrencyConverterTest {
+
+    CurrencyConverter c = new CurrencyConverter();
+
+    @Test
+    public void testRupeeToDollar() {
+        assertEquals(1, c.rupeeToDollar(83), 0.1);
+    }
+
+    @Test
+    public void testDollarToRupee() {
+        assertEquals(83, c.dollarToRupee(1), 0.1);
+    }
+}
